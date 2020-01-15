210ef5547ddfbbc8c4f84a6b2d76dd7f593be6d2
Meero Harootunian <meero.harootunian@gmail.com>
2020-01-14 22:59:32 -0700

    Updates

--- README.md
+++
@@ -1,9 +0,0 @@
-##Clicky Game
-
-###How to play
-When the game loads, you will see 12 images on the screen. Click on any image to gain 1 point. Everytime you click an image, all the images on the page
-will shuffle around. This is a memory game so remember what images you have clicked! 
-
-If you click the same image twice in one round, you lose and must start over.
-
-The game can be [accessed here](https://mharootunian.github.io/bc-clicky-game)
---
+++ precache-manifest.359ae435887aae79294b3bf51acc2c32.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f62a70beb2beb28edb97818cac87360",
+    "url": "/bc-clicky-game/index.html"
+  },
+  {
+    "revision": "aa882653c20647691603",
+    "url": "/bc-clicky-game/static/css/main.221a97bd.chunk.css"
+  },
+  {
+    "revision": "5f90dbdc450090083790",
+    "url": "/bc-clicky-game/static/js/2.3460f776.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/bc-clicky-game/static/js/2.3460f776.chunk.js.LICENSE"
+  },
+  {
+    "revision": "aa882653c20647691603",
+    "url": "/bc-clicky-game/static/js/main.223a5196.chunk.js"
+  },
+  {
+    "revision": "204d1274f56fab7d95e8",
+    "url": "/bc-clicky-game/static/js/runtime-main.c5a43933.js"
+  }
+]);
